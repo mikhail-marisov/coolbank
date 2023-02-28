e6f8565e63e96abd89f2ce14e71282f1a70005f2
Mikhail_Marisov <marisov.mikhail@icloud.com>
2023-02-28 14:06:08 +0300

    Initial commit

---
+++ src/main/java/com/example/coolbank/CoolbankApplication.java
@@ -0,0 +1,13 @@
+package com.example.coolbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coolbank/CoolbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coolbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
